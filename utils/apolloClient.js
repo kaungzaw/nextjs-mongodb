f3fe3872401fb1f925f3c426f81d41a3a973e96c
Kaung Zaw <kaungzaw@immin.io>
2022-09-09 20:15:58 +0630

    Change graphql endpoint

--- utils/apolloClient.js
+++ utils/apolloClient.js
@@ -1,10 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const apolloClient = new ApolloClient({
-  uri:
-    process.env.VERCEL_ENV === "production"
-      ? "https://nextjs-mongodb-smoky.vercel.app/api/graphql"
-      : "http://localhost:3000/api/graphql",
+  uri: "https://nextjs-mongodb-smoky.vercel.app/api/graphql",
   cache: new InMemoryCache(),
 });
 
